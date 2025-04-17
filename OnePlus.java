ddecfcf7989a21e2ba5d03f3b2066979fb41c50d
sakshigmail <sakshi3016kaurav.com>
2025-04-17 21:56:40 +0530

    changes

--- OnePlus.java
+++ OnePlus.java
@@ -1,6 +1,31 @@
 public class OnePlus {
+    public int[] plusOne(int[] digits){
+        // 1, 2, 3
+        //      2 ,3 ,4
+        for(int i = digits.length - 1; i >= 0; i--){
+            if(digits[i] != 9){
+                digits[i] += 1;
+                return digits;
+            }else{
+                digits[i] = 0;
+            }
+        }
+        int[] newArr = new int[digits.length + 1];
+        newArr[0] = 1;
+        digits = newArr;
+        return digits;
+
+    }
 
     public static void main(String[] args) {
         // this is a solution for leetcode problem OnePlus
+        OnePlus obj = new OnePlus();
+        int[] digits = {9, 9, 9};
+        int[] result = obj.plusOne(digits);
+
+        for(int num : result){
+            System.out.print(num + " ");
+        }
+
     }
 }
