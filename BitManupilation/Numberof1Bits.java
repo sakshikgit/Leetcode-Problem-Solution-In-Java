08728bef0b93de7531fa12c94f351234835e98f2
sakshigmail <sakshi3016kaurav.com>
2025-08-11 18:28:21 +0530

    changes

--- BitManupilation/Numberof1Bits.java
+++ BitManupilation/Numberof1Bits.java
@@ -12,27 +12,38 @@ public class Numberof1Bits {
 
 
    
-    public static int hammingWeight(int n) {
-         StringBuilder sb = new StringBuilder();
+    // public static int hammingWeight(int n) {
+    //      StringBuilder sb = new StringBuilder();
     
-        while(n >0){
-            int rem = n%2;
-            sb.append(rem);
-            n = n/2;
+    //     while(n >0){
+    //         int rem = n%2;
+    //         sb.append(rem);
+    //         n = n/2;
 
-        }
-        sb = sb.reverse();
+    //     }
+    //     sb = sb.reverse();
         
-        int setBit = 0;
-        for(int i=0; i<sb.length(); i++){
-            if(sb.charAt(i) == '1'){
-                setBit++;
-            }
+    //     int setBit = 0;
+    //     for(int i=0; i<sb.length(); i++){
+    //         if(sb.charAt(i) == '1'){
+    //             setBit++;
+    //         }
+    //     }
+    //     // System.out.println(setBit);
+    //     return setBit;
+    // }
+        
+     public static int hammingWeight(int n) {
+        int count  = 0;
+        while(n!=0){
+            count += (n & 1);
+            n >>= 1;
         }
-        // System.out.println(setBit);
-        return setBit;
+        return count;
+        
     }
-        
+    // N & 1 gives last digit
+    // >>> to handel negative numbers as well
     
 
 
